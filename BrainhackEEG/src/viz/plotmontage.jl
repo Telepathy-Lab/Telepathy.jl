406ea9e819bfc7aaf7905fc60956157a960c5c66
TKostka <tomasz.d.kostka@gmail.com>
2022-03-27 12:52:55 +0200

    added nose :)

--- BrainhackEEG/src/viz/plotmontage.jl
+++ BrainhackEEG/src/viz/plotmontage.jl
@@ -2,7 +2,7 @@ function plotmontage(data::RawEEG)
     set_montage(data)
     f = Figure(resolution=(800,800))
     ax = Axis(f[1, 1], aspect=1)
-
+    poly!(Point2f[(-10,90), (10,90), (0, 100)], color = :white, strokecolor = :black, strokewidth = 1)
     poly!(Circle(Point2f(0, 0), 90), color = :white, strokewidth = 1, strokecolor = :black)
     for i in data.chans
         t=(((100-i[2][3])/100)^-0.5)*1.1
