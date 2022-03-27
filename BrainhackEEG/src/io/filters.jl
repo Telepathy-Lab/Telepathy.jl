b1a12a934ae5a396fe2db003d62929c9e4fe8523
Neurux <101219831+Neurux@users.noreply.github.com>
2022-03-27 14:49:26 +0200

    appling notch and hipass filter to all channels
    Signed-off-by: Neurux <101219831+Neurux@users.noreply.github.com>

--- BrainhackEEG/src/io/filters.jl
+++ BrainhackEEG/src/io/filters.jl
@@ -1,15 +1,3 @@
-
-"""
-Notch filter for single signal vector.
-By default filters out 50Hz power interference.
-fs - sampling frequency of the input signal (defaults to 2048)
-fw - frequency to be filtered out (defaults to 50Hz)
-bw - bandwidth of the filter (default 0.12 works well for 50Hz power)
-"""
-function notch_filter(input_signal; fs=2048, fw=50, bw=0.12) 
-    return filt(iirnotch(fw, bw; fs=fs), input_signal)
-end
-
 """
 Highpass filter for single signal vector.
 fs - sampling frequency of the input signal (defaults to 2048)
@@ -20,6 +8,17 @@ function highpass_filter(input_signal, fw; fs=2048, hw=511)
     responsetype = Highpass(fw; fs=fs)
     designmethod = FIRWindow(hanning(hw))
     return filt(digitalfilter(responsetype, designmethod), input_signal)
+end
+
+"""
+Notch filter for single signal vector.
+By default filters out 50Hz power interference.
+fs - sampling frequency of the input signal (defaults to 2048)
+fw - frequency to be filtered out (defaults to 50Hz)
+bw - bandwidth of the filter (default 0.12 works well for 50Hz power)
+"""
+function notch_filter(input_signal; fs=2048, fw=50, bw=0.12) 
+    return filt(iirnotch(fw, bw; fs=fs), input_signal)
 end
 
 """
@@ -45,6 +44,39 @@ function apply_lowpass_filter(data::RawEEG, prog; hw=511)
     new_signals = Array{Float32}(undef, (no_samples,no_channels))
     for channel in 1:no_channels
         new_signals[:,channel] = lowpass_filter(data.data[:,channel], prog, fs=fs, hw=hw)
+    end
+    new_data = RawEEG(data.info, new_signals, data.status, data.lowTrigger, data.highVector, data.chans)
+    return new_data
+end
+
+"""
+Applies notch filter to the whole dataset.
+Returns new dataset with transformed data.
+"""
+function apply_notch_filter(data::RawEEG; fw=50, bw=0.12) 
+    fs = data.info["sampRate"][1]
+    no_channels = size(data.data)[2]
+    no_samples = size(data.data)[1]
+    new_signals = Array{Float32}(undef, (no_samples,no_channels))
+    for channel in 1:no_channels
+        new_signals[:,channel] = notch_filter(data.data[:,channel],fw=fw, fs=fs, bw=bw)
+    end
+    new_data = RawEEG(data.info, new_signals, data.status, data.lowTrigger, data.highVector, data.chans)
+    return new_data
+end
+
+
+"""
+Applies highpass filter to the whole dataset.
+Returns new dataset with transformed data.
+"""
+function apply_highpass_filter(data::RawEEG, fw; hw=511)
+    fs = data.info["sampRate"][1]
+    no_channels = size(data.data)[2]
+    no_samples = size(data.data)[1]
+    new_signals = Array{Float32}(undef, (no_samples,no_channels))
+    for channel in 1:no_channels
+        new_signals[:,channel] = highpass_filter(data.data[:,channel], fw, fs=fs, hw=hw)
     end
     new_data = RawEEG(data.info, new_signals, data.status, data.lowTrigger, data.highVector, data.chans)
     return new_data
