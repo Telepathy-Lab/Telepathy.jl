c85ccb747435c73b2f75eeb243672537433f0150
Marcin Koculak <koculak.marcin@gmail.com>
2023-02-17 00:45:12 +0100

    Allow for plotting more filters at once

--- src/viz/plot_filter.jl
+++ src/viz/plot_filter.jl
@@ -1,12 +1,39 @@
-function plot_filter(filter::Vector, srate; 
+function plot_freq(axis, xNum, xPoints, coefs, color)
+    fResp = freqresp(coefs, 0:π/xNum:π)
+    fResp = 20*log10.(abs.(fResp))
+    lines!(axis, xPoints, fResp, linewidth=0.75, color=color)
+end
+
+function plot_phase(axis, xNum, xPoints, coefs, color)
+    pResp = phaseresp(coefs, 0:π/xNum:π)
+    lines!(axis, xPoints, pResp, linewidth=0.75, color=color)
+end
+
+function plot_group(axis, xNum, xPoints, coefs, color)
+    gDelay = grpdelay(coefs, 0:π/xNum:π)
+    lines!(axis, xPoints, gDelay/2048, linewidth=0.75, color=color)
+    ylims!(axis, -1,1)
+end
+
+function plot_impulse(axis, xNum, xPoints, coefs, color)
+    samples = length(coefs.b.coeffs)
+    stem!(axis, impresp(coefs, samples), markersize=1, linewidth=0.5, color=color)
+end
+
+function plot_step(axis, xNum, xPoints, coefs, color)
+    samples = length(coefs.b.coeffs)
+    lines!(axis, stepresp(coefs, samples), color=color)
+end
+
+
+plot_filter(filterCoefs::Vector, srate; kwargs...) = plot_filters([filterCoefs], srate, kwargs...)
+
+function plot_filters(filters::Vector, srate; 
                     freq::Bool=true, phase::Bool=true, 
                     group::Bool=false, impulse::Bool=false, step::Bool=false)
-
+    
     fig = Figure()
     axNum = 1
-
-    # Transform the vector with filter values to coefficients expected by response functions
-    coefs = PolynomialRatio(filter, [1.])
 
     # Estimate the number of points to make a smooth plot (> 5000)
     if srate < 10000
@@ -18,52 +45,25 @@ function plot_filter(filter::Vector, srate;
     xNum = multiplier*srate
     xPoints = (0:1/multiplier:srate)/2
 
-    # Frequency response
-    if freq
-        freqAx = fig[axNum,1] = Axis(fig)
-        axNum += 1
+    # Create axes for requested panels
+    freq && (freqAx = fig[axNum,1] = Axis(fig); axNum += 1)
+    phase && (phaseAx = fig[axNum,1] = Axis(fig); axNum += 1)
+    group && (groupAx = fig[axNum,1] = Axis(fig); axNum += 1)
+    impulse && (impAx = fig[axNum,1] = Axis(fig); axNum += 1)
+    step && (stepAx = fig[axNum,1] = Axis(fig); axNum += 1)
 
-        
-        fResp = freqresp(coefs, 0:π/xNum:π)
-        fResp = 20*log10.(abs.(fResp))
-        lines!(freqAx, xPoints, fResp, linewidth=0.75, color=:black)
+    color=[:black, :red]
+
+    for (idx, filterCoefs) in enumerate(filters)
+        # Transform the vector with filter values to coefficients expected by response functions
+        coefs = PolynomialRatio(filterCoefs, [1.])
+
+        freq && plot_freq(freqAx, xNum, xPoints, coefs, color[idx])
+        phase && plot_phase(phaseAx, xNum, xPoints, coefs, color[idx])
+        group && plot_group(groupAx, xNum, xPoints, coefs, color[idx])
+        impulse && plot_impulse(impAx, xNum, xPoints, coefs, color[idx])
+        step && plot_step(stepAx, xNum, xPoints, coefs, color[idx])
     end
 
-    # Phase response
-    if phase
-        phaseAx = fig[axNum,1] = Axis(fig)
-        axNum += 1
-
-        pResp = phaseresp(coefs, 0:π/xNum:π)
-        lines!(phaseAx, xPoints, pResp, linewidth=0.75, color=:black)
-    end
-
-    # Group delay
-    if group
-        groupAx = fig[axNum,1] = Axis(fig)
-        axNum += 1
-
-        gDelay = grpdelay(coefs, 0:π/xNum:π)
-        lines!(groupAx, xPoints, gDelay/2048, linewidth=0.75, color=:black)
-        ylims!(groupAx, -0.2,0.2)
-    end
-
-    # Impulse response
-    if impulse
-        impAx = fig[axNum,1] = Axis(fig)
-        axNum += 1
-
-        samples = length(filter)
-        stem!(impAx, impresp(coefs, samples), markersize=1, linewidth=0.5)
-    end
-
-    # Step response
-    if step
-        stepAx = fig[axNum,1] = Axis(fig)
-        axNum += 1
-
-        samples = length(filter)
-        lines!(stepAx, stepresp(coefs, samples))
-    end
     display(fig)
 end
