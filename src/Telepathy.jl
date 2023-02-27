a533ea08f2c31dd3802f04987c6340fccc82a144
Marcin Koculak <koculak.marcin@gmail.com>
2023-02-27 03:24:45 +0100

    remove duplicates

--- src/Telepathy.jl
+++ src/Telepathy.jl
@@ -37,13 +37,6 @@ export channel_names, get_channels, set_type!
 include("channels/layout.jl")
 export read_layout, set_layout!
 
-include("viz/plot_layout.jl")
-export plot_layout
-
-include("viz/plot_raw.jl")
-include("viz/plot_filter.jl")
-export plot_filter
-
 include("preprocessing/filter.jl")
 export filter_data, filter_data!, design_filter
 
